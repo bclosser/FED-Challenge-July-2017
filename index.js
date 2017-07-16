c46967818e15d05bd36144534b29e632a58f524d
Barry Closser <barry.closser@gmail.com>
2017-07-16 14:07:30 -0500

    Load Pizza.json and handle error

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import fetch from 'isomorphic-fetch';
 
 // Note: this is the entry point for the entire application
+let pizzaList = [];
+let loadingPizzas = true;
 
 // step 1: you will need to load the pizza data. it is available at /pizza.json. what-wg fetch is pre-installed.
 // remember that fetch uses promises.
+
+fetch('/pizza.json')
+	.then(response => {
+		if (response.status >= 400) {
+            alert("Error Loading Pizzas");
+			throw new Error("Unable to load pizzas");
+		}
+		return response.json();
+	})
+	.then(pizzas => {
+        pizzaList = pizzas.pizzas;
+        loadingPizzas = false;
+	});
 
 // step 2: implement the view and required behaviors
